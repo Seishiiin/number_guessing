338314c35693a329869c96ab59f03feafff67028
Seishin <gabinhalloss@gmail.com>
2024-05-18 00:18:48 +0200

    add number of tests

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import random
 
 
-def number_guessing(i=random.randint(0, 100)):
+def number_guessing(i=random.randint(0, 100), y=0):
     x = int(input("Essayez de trouver le nombre (entre 0 et 100) : "))
     while i != x:
         print("Le nombre est plus petit") if x > i else print("Le nombre est plus grand")
+        y += 1
         x = int(input("Essayez à nouveau : "))
 
-    return f"Bien joué tu as trouver le nombre {i}"
+    return f"Bien joué tu as trouver le nombre {i} en {y} essai(s)"
 
 
 print(number_guessing())
